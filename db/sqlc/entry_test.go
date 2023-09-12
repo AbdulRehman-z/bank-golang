d7fd9f2421730e11e112c750ef0dd93005082108
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-09-12 22:02:40 +0500

    entries test are passing

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,1 +1,56 @@
 package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/AbdulRehman-z/bank-golang/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+
+	args := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    int64(util.GenerateRandomMoney()),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, args.Amount, entry.Amount)
+	require.Equal(t, args.AccountID, entry.AccountID)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry := createRandomEntry(t, account)
+
+	require.NotEmpty(t, entry)
+}
+
+func TestListEntries(t *testing.T) {
+
+	account := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	args := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), args)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
