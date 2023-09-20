a8443fb6257306970c36739e7647192a2a382993
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-09-20 19:29:09 +0500

    env's loaded

--- main.go
+++ main.go
@@ -4,23 +4,31 @@ import (
 	"database/sql"
 	"flag"
 	"log"
+	"os"
 
 	"github.com/AbdulRehman-z/bank-golang/api"
 	db "github.com/AbdulRehman-z/bank-golang/db/sqlc"
 	"github.com/AbdulRehman-z/bank-golang/util"
+	"github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:password@localhost:5432/bankDb?sslmode=disable"
 )
 
 func main() {
 	util.ClearConsole()
 	listenAddr := flag.String("listenaddr", ":8080", "server listen address")
 	flag.Parse()
+
+	// Load env variables
+	godotenv.Load(".env")
+	dbDriver, exists := os.LookupEnv("DB_DRIVER")
+	if !exists {
+		log.Fatal("DB_DRIVER environment variable not set")
+	}
+	dbSource, exists := os.LookupEnv("DB_URL")
+	if !exists {
+		log.Fatal("DB_URL environment variable not set")
+	}
 
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
