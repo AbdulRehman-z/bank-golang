f756134b529926269d872666ef7c289dfbe77aa1
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-09-20 16:07:24 +0500

    updated params

--- types/account.go
+++ types/account.go
@@ -1,6 +1,32 @@
 package types
 
-type CreateAccountRequest struct {
-	Owner    string `validate:"required,min=5,max=20"`      // Required field, min 5 char long max 20
-	Currency string `validate:"required,oneof=USD EUR CAD"` // Required field, and client needs to implement our 'teener' tag format which we'll see later
-}
+type (
+	// CreateAccountRequest defines the request body for creating an account
+	CreateAccountRequest struct {
+		Owner    string `validate:"required,min=5,max=20"`      // Required field, min 5 char long max 20
+		Currency string `validate:"required,oneof=USD EUR CAD"` // Required field, one of USD, EUR, CAD
+	}
+
+	// GetAccountRequest defines the request body for getting an account
+	// id must be positive and grater than 0
+	GetAccountRequest struct {
+		ID int64 `validate:"required,gt=0"` // Required field, min 1
+	}
+
+	// ListAccountsRequest defines the request body for listing accounts
+	ListAccountsRequest struct {
+		PageID   int32 `validate:"required,gt=1"` // Required field, min 1
+		PageSize int32 `validate:"required,lt=5"` // Required field, min 5
+	}
+
+	// UpdateAccountRequest defines the request body for updating an account
+	UpdateAccountRequest struct {
+		ID      int64 `validate:"required,min=1"` // Required field, min 1
+		Balance int64 `validate:"required,min=1"` // Required field, min 1
+	}
+
+	// DeleteAccountRequest defines the request body for deleting an account
+	DeleteAccountRequest struct {
+		ID int64 `validate:"required,min=1"` // Required field, min 1
+	}
+)
