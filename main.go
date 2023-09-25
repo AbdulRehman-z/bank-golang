991f5dd679aca43377f5ce2bc37ae4238dad259e
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-09-25 22:27:25 +0500

    experiment with task scheduling

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/AbdulRehman-z/bank-golang/api"
 	db "github.com/AbdulRehman-z/bank-golang/db/sqlc"
@@ -34,9 +37,32 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to connect: ", err)
 	}
-
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 	// fmt.Println("Starting server on")
 	log.Fatal(server.Start(*listenAddr))
+
+	// long pooling after every 24 hours to update exchange rates, save them in db
+	ticker := time.NewTicker(24 * time.Hour)
+	for {
+		select {
+		case <-ticker.C:
+			// save new exchange rates in db
+			url := "https:openexchangerates.org/api/latest.json?app_id=a7784caacbb24a7b9e6129733000733a"
+			// get exchange rates from api
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			if err != nil {
+				panic(err)
+			}
+			client := &http.Client{}
+			response, err := client.Do(request)
+			if err != nil {
+				panic(err)
+			}
+			defer response.Body.Close()
+			// save exchange rates in db
+			fmt.Printf("response status: %v\n", response.Body)
+		}
+	}
+
 }
