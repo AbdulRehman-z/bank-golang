34683ba992b3a9cd294b5d54db563f309a426605
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-09-25 19:25:30 +0500

    add tags

--- types/account.go
+++ types/account.go
@@ -10,23 +10,23 @@ type (
 	// GetAccountRequest defines the request body for getting an account
 	// id must be positive and grater than 0
 	GetAccountRequest struct {
-		ID int64 `validate:"required,gt=0"` // Required field, min 1
+		ID int64 `json:"id" validate:"required,gt=0"` // Required field, min 1
 	}
 
-	// ListAccountsRequest defines the request body for listing accounts
+	// ListAccountsRequest defines the request params for listing accounts
 	ListAccountsRequest struct {
-		PageID   int32 `validate:"required,gte=1"` // Required field, min 1
-		PageSize int32 `validate:"required,lte=5"` // Required field, min 5
+		PageID   int32 `query:"page_id" validate:"required,gte=1"`   // Required field, min 1
+		PageSize int32 `query:"page_size" validate:"required,lte=5"` // Required field, min 5
 	}
 
 	// UpdateAccountRequest defines the request body for updating an account
 	UpdateAccountRequest struct {
-		ID      int64 `validate:"required,min=1"` // Required field, min 1
-		Balance int64 `validate:"required,min=1"` // Required field, min 1
+		ID      int64 `json:"id" validate:"required,min=1"`      // Required field, min 1
+		Balance int64 `json:"balance" validate:"required,min=1"` // Required field, min 1
 	}
 
 	// DeleteAccountRequest defines the request body for deleting an account
 	DeleteAccountRequest struct {
-		ID int64 `validate:"required,min=1"` // Required field, min 1
+		ID int64 `json:"id" validate:"required,min=1"` // Required field, min 1
 	}
 )
