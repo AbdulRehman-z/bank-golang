8e73336a8ed9e925bf13df7e488dd8100b784f11
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-09-28 13:49:22 +0500

    user tests pass

--- api/user_test.go
+++ api/user_test.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"testing"
@@ -26,8 +28,8 @@ func TestCreateUserAPI(t *testing.T) {
 		checkResponse func(t *testing.T, response *http.Response)
 	}{
 		{
-			name: "OK",
-			body: types.CreateUserParams{
+			name: "CreateUserSuccess",
+			body: types.CreateUserRequest{
 				Username:       util.GenerateRandomOwnerName(),
 				FullName:       util.GenerateRandomOwnerName(),
 				Email:          util.GenerateRandomEmail(),
@@ -42,6 +44,71 @@ func TestCreateUserAPI(t *testing.T) {
 			checkResponse: func(t *testing.T, response *http.Response) {
 				require.Equal(t, http.StatusCreated, response.StatusCode)
 				requireUserBodyMatch(t, response.Body, user)
+			},
+		},
+		{
+			name: "InvalidRequestBody",
+			body: "akd",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				require.Equal(t, http.StatusBadRequest, response.StatusCode)
+			},
+		},
+		{
+			name: "InvalidRequestFields",
+			body: types.CreateUserRequest{
+				Username:       util.GenerateRandomOwnerName(),
+				FullName:       util.GenerateRandomOwnerName(),
+				Email:          util.GenerateRandomEmail(),
+				HashedPassword: "se",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				require.Equal(t, http.StatusBadRequest, response.StatusCode)
+			},
+		},
+		{
+			name: "InternalError",
+			body: types.CreateUserRequest{
+				Username:       util.GenerateRandomOwnerName(),
+				FullName:       util.GenerateRandomOwnerName(),
+				Email:          util.GenerateRandomEmail(),
+				HashedPassword: "secretrrr",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.User{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				require.Equal(t, http.StatusInternalServerError, response.StatusCode)
+			},
+		},
+		{
+			name: "UserAlreadyExists",
+			body: types.CreateUserRequest{
+				Username:       util.GenerateRandomOwnerName(),
+				FullName:       util.GenerateRandomOwnerName(),
+				Email:          util.GenerateRandomEmail(),
+				HashedPassword: "secretrrr",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.User{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				require.Equal(t, http.StatusBadRequest, response.StatusCode)
 			},
 		},
 	}
@@ -71,12 +138,101 @@ func TestCreateUserAPI(t *testing.T) {
 	}
 }
 
+func TestGetUserAPI(t *testing.T) {
+	user := randomUser()
+
+	testCases := []struct {
+		name          string
+		body          interface{}
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, response *http.Response)
+	}{
+		{
+			name: "GetUserSuccess",
+			body: "akd",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(user, nil)
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				require.Equal(t, http.StatusOK, response.StatusCode)
+				requireUserBodyMatch(t, response.Body, user)
+			},
+		},
+		{
+			name: "InternalError",
+			body: "akd",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.User{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				require.Equal(t, http.StatusInternalServerError, response.StatusCode)
+			},
+		},
+		{
+			name: "UserNotFound",
+			body: "akd",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.User{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				require.Equal(t, http.StatusNotFound, response.StatusCode)
+			},
+		},
+		{
+			name: "InvalidParams",
+			body: "a",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				require.Equal(t, http.StatusBadRequest, response.StatusCode)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		ctrl := gomock.NewController(t)
+		ctrl.Finish()
+
+		store := mockdb.NewMockStore(ctrl)
+		server := NewServer(store)
+		tc.buildStubs(store)
+
+		// get username parameter from body
+		username := tc.body.(string)
+
+		url := "/users/" + username
+
+		request, err := http.NewRequest(http.MethodGet, url, nil)
+		fmt.Printf("url: %v\n", url)
+		require.NoError(t, err)
+
+		response, err := server.router.Test(request)
+		require.NoError(t, err)
+
+		tc.checkResponse(t, response)
+	}
+}
+
 func randomUser() db.User {
 	return db.User{
 		Username:       util.GenerateRandomOwnerName(),
 		FullName:       util.GenerateRandomOwnerName(),
 		Email:          util.GenerateRandomEmail(),
-		HashedPassword: "secret",
+		HashedPassword: "secreterr",
 	}
 }
 
