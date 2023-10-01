f01203773f7ba440aa2a54f375190f1307d6464a
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-10-01 15:49:55 +0500

    remove obselete code

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"flag"
-	"fmt"
 	"log"
-	"net/http"
-	"time"
 
 	"github.com/AbdulRehman-z/bank-golang/api"
 	db "github.com/AbdulRehman-z/bank-golang/db/sqlc"
@@ -17,9 +13,6 @@ import (
 
 func main() {
 	util.ClearConsole()
-	listenAddr := flag.String("listenaddr", ":8080", "server listen address")
-	flag.Parse()
-
 	// Load env variables
 	config, err := util.LoadConfig(".")
 	if err != nil {
@@ -34,30 +27,5 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to create server: ", err)
 	}
-	// fmt.Println("Starting server on")
-	log.Fatal(server.Start(*listenAddr))
-
-	// long pooling after every 24 hours to update exchange rates, save them in db
-	ticker := time.NewTicker(24 * time.Hour)
-	for {
-		select {
-		case <-ticker.C:
-			// save new exchange rates in db
-			url := "https:openexchangerates.org/api/latest.json?app_id=a7784caacbb24a7b9e6129733000733a"
-			// get exchange rates from api
-			request, err := http.NewRequest(http.MethodGet, url, nil)
-			if err != nil {
-				panic(err)
-			}
-			client := &http.Client{}
-			response, err := client.Do(request)
-			if err != nil {
-				panic(err)
-			}
-			defer response.Body.Close()
-			// save exchange rates in db
-			fmt.Printf("response status: %v\n", response.Body)
-		}
-	}
-
+	log.Fatal(server.Start(&config.LISTEN_ADDR))
 }
