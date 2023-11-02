68263ad57dd2bca388dc0dfbc05dc39e909d5e87
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-11-02 07:41:35 -0700

    add http logger

--- main.go
+++ main.go
@@ -143,7 +143,8 @@ func runGatewayServer(config *util.Config, store db.Store) {
 	}
 
 	log.Info().Msg("Starting grpc gateway server on " + config.GRPC_GATEWAY_ADDR)
-	err = http.Serve(listener, mux)
+	handler := gapi.HttpLogger(mux)
+	err = http.Serve(listener, handler)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to serve")
 	}
