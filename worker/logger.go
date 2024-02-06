39d8a43c5d3fd93aea27c7a9de8a9636241310d1
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-02-06 21:31:49 +0500

    update interface => any

--- worker/logger.go
+++ worker/logger.go
@@ -14,26 +14,26 @@ func NewLogger() *Logger {
 	return &Logger{}
 }
 
-func (l *Logger) Print(level zerolog.Level, args ...interface{}) {
+func (l *Logger) Print(level zerolog.Level, args ...any) {
 	log.WithLevel(level).Msg(fmt.Sprint(args...))
 }
 
-func (l *Logger) Info(args ...interface{}) {
+func (l *Logger) Info(args ...any) {
 	l.Print(zerolog.InfoLevel, args...)
 }
 
-func (l *Logger) Error(args ...interface{}) {
+func (l *Logger) Error(args ...any) {
 	l.Print(zerolog.ErrorLevel, args...)
 }
 
-func (l *Logger) Fatal(args ...interface{}) {
+func (l *Logger) Fatal(args ...any) {
 	l.Print(zerolog.FatalLevel, args...)
 }
 
-func (l *Logger) Debug(args ...interface{}) {
+func (l *Logger) Debug(args ...any) {
 	l.Print(zerolog.DebugLevel, args...)
 }
 
-func (l *Logger) Warn(args ...interface{}) {
+func (l *Logger) Warn(args ...any) {
 	l.Print(zerolog.WarnLevel, args...)
 }
